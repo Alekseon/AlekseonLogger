e7203074468b5c2e984a23a3185237d44ba97bc9
marcinfr <m.frymark@alekseon.com>
2018-12-11 12:04:20 +0100

    fix for production mode

--- Handler.php
+++ Handler.php
@@ -15,6 +15,24 @@ class Handler extends \Magento\Framework\Logger\Handler\Base
 {
     protected $fileName = '/var/log/alekseon.log';
 
+    /**
+     * Handler constructor.
+     * @param \Magento\Framework\Filesystem\Driver\File $filesystem
+     * @param null $filePath
+     * @param null $fileName
+     */
+    public function __construct(
+        \Magento\Framework\Filesystem\Driver\File $filesystem,
+        $filePath = null,
+        $fileName = null
+    ) {
+        parent::__construct($filesystem, $filePath, $fileName);
+    }
+
+    /**
+     * @param $fileName
+     * @throws \Exception
+     */
     public function setFileName($fileName)
     {
         $this->fileName = BP . DIRECTORY_SEPARATOR . '/var/log/' . $fileName;
